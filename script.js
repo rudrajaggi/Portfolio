3068e2055b0f018b8d5b936d03550e5a4b56b511
rudrajaggi <rudrajaggi@gmail.com>
2025-11-27 17:46:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello World");
+fullName = "Rudra";
+console.log(fullName);
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+};
